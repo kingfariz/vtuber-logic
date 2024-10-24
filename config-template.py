0d0a2fa1739bab5097c3b4f26ca21303e03404f4
kingfariz <kungfupanda22@gmail.com>
2024-10-25 01:23:00 +0700

    initialized from iseekaigo repo

---
+++ config-template.py
@@ -0,0 +1,9 @@
+# CREATOR MODE:
+# max_token = 200
+# maxCharacters = 200
+# status_config = "CREATOR_MODE"
+
+## CHAT WITH VIWER MODE
+# max_token = 50
+# maxCharacters = 20
+# status_config = "VIEWER_MODE"
---
+++ run-docker.sh
@@ -0,0 +1,1 @@
+winpty docker run --rm -it -p '127.0.0.1:50021:50021' voicevox/voicevox_engine:cpu-ubuntu20.04-latest
