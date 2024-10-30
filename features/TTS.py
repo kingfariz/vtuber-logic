914411ffe119510b17cc1146bfce6d49a86e1b96
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2024-10-30 02:17:33 -0700

    Change JP TTS to `もち子さん Normal`
    id: 20
    
    Needs credit: `VOICEVOX:もち子(cv 明日葉よもぎ)`
    
    https://voicevox.hiroshiba.jp/product/mochikosan/

--- features/TTS.py
+++ features/TTS.py
@@ -3,16 +3,16 @@ import urllib.parse
 # from utils.katakana import *
 
 
-def voicevox_tts(text: str, output_path="output.wav"):
+def voicevox_tts(text: str, speaker_id=20, output_path="output.wav"):
     # You need to run VoicevoxEngine.exe or voicevox docker first before running this script
     voicevox_url = 'http://localhost:50021'
     # Convert the text to katakana. Example: ORANGE -> オレンジ, so the voice will sound more natural
     # katakana_text = katakana_converter(tts)
     # You can change the voice to your liking. You can find the list of voices on speaker.json
     # or check the website https://voicevox.hiroshiba.jp
-    params_encoded = urllib.parse.urlencode({'text': text, 'speaker': 46})
+    params_encoded = urllib.parse.urlencode({'text': text, 'speaker': speaker_id})
     request = requests.post(f'{voicevox_url}/audio_query?{params_encoded}')
-    params_encoded = urllib.parse.urlencode({'speaker': 46, 'enable_interrogative_upspeak': True})
+    params_encoded = urllib.parse.urlencode({'speaker': speaker_id, 'enable_interrogative_upspeak': True})
     request = requests.post(f'{voicevox_url}/synthesis?{params_encoded}', json=request.json())
 
     with open(output_path, "wb") as outfile:
