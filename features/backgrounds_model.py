ff4960c349c8ec6b8c08799c73099f44e335f240
kingfariz <kungfupanda22@gmail.com>
2024-11-03 20:33:32 +0700

    feat: add models

---
+++ features/backgrounds_model.py
@@ -0,0 +1,7 @@
+from enum import Enum
+
+class BackgroundEnum(str, Enum):
+    Default = "Default"
+    Iseekaigo = "Iseekaigo"
+    Ninjacat = "Ninjacat"
+    Perfumes = "Perfumes"
---
+++ features/product_model.py
@@ -0,0 +1,13 @@
+from enum import Enum
+
+class ProductEnum(str, Enum):
+    AURUM_DUSK = "Aurum_Dusk"
+    CELESTE_JARDIN = "Celeste_Jardin"
+    NOIR_ROUGE = "Noir_Rouge"
+    VERDE_LUXE = "Verde_Luxe"
+    AZURE_MIRAGE = "Azure_Mirage"
+    IVORY_BLOOM = "Ivory_Bloom"
+    CAFE_NOIR = "Café_Noir"
+    LUNA_EMBRACE = "Luna_Embrace"
+    AMBER_MYSTIQUE = "Amber_Mystique"
+    BLUSH_REVERIE = "Blush_Reverie"
