10494fd3367a47b44b50fcd7c8db447b211b27b3
chengxulan <lanchengxu0807@gmail.com>
2024-11-02 18:06:29 -0700

    Add games data and modify prompt

--- features/prompt_maker.py
+++ features/prompt_maker.py
@@ -1,27 +1,36 @@
 import pandas as pd
 from config import *
 
-# sys.stdout = open(sys.stdout.fileno(), mode='w', encoding='utf8', buffering=1)
-
 def get_identity(identity_path) -> str:
     with open(identity_path, "r", encoding="utf-8") as f:
         identity_context = f.read()
     
     return identity_context
 
+def parse_product_data(csv_path="data/products/perfumes.csv", n_rows=10):
+    df = pd.read_csv(csv_path,encoding="ISO-8859-1")
+    df = df.head(n_rows)
+    # format: list[dict[col: str, val: str]]
+    product_json_string = df.to_json(orient='records')
+    return product_json_string
+
 def get_system_prompt(
     identity_path="characterConfig/Character/shopping_assistant.txt",
 ) -> dict[str, str]:
     identity_context = get_identity(identity_path)
+    perfumes_list = parse_product_data()
+    games_list = parse_product_data(csv_path="data/products/games.csv")
+    product_prompt = f"\nHere is the product list:\n{perfumes_list} {games_list}"
+    system_prompt_str = identity_context + product_prompt
 
-    return {"role": "system", "content": identity_context}
+    return {"role": "system", "content": system_prompt_str}
     
 def get_prompt(history: list[str]):
     total_len = 0
 
     # Get Vtuber identity/role
-    identity = get_identity()
-    prompt = [identity] + history
+    system_prompt = get_system_prompt()
+    prompt = [system_prompt] + history
 
     # Step 3: Add a system message for response constraints
     if status_config == "VIEWER_MODE":
@@ -45,6 +54,6 @@ def get_prompt(history: list[str]):
     return prompt
 
 if __name__ == "__main__":
-    prompt = get_prompt()
+    history = []
+    prompt = get_prompt(history=history)
     print(prompt)
-    print(len(prompt))
