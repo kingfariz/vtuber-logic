fb7b822e7c54fc32d55c6d50f4b98299cf5b2f96
Kazuki Yoda <yd.tokyu9006f@gmail.com>
2024-11-02 23:31:43 -0700

    Disable `max_tokens` for OpenAI API outputs

---
+++ config.py
@@ -0,0 +1,9 @@
+# CREATOR MODE:
+max_token = None # Disable. Do NOT limit the output tokens for chat completion.
+maxCharacters = 200
+status_config = "CREATOR_MODE"
+
+## CHAT WITH VIWER MODE
+# max_token = 4096
+# maxCharacters = 20000
+# status_config = "VIEWER_MODE"
